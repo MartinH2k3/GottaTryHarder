3fa4ddcfe187b6610fffd54bcb1e05eaefb1d665
martinh2k3 <martin.horsky.2003@gmail.com>
2025-09-09 21:52:39 +0200

    feat: Helper class with helper methods

---
+++ Assets/Scripts/Utils/Helpers.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+public static class Helpers
+{
+    public static bool LayerInLayerMask(int layer, LayerMask layerMask)
+    {
+        return (layerMask & (1 << layer)) != 0;
+    }
+}
+}
