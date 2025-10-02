1a2390110ee1eb0216fd21b836d6fe28ba057760
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-10-02 11:52:50 +0200

    feat: working on how statistics are stored

---
+++ Assets/Scripts/Infrastructure/Storage/DataModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Infrastructure.Storage
+{
+
+/// <summary> Per-level stats </summary>
+public class LevelStats
+{
+    public int LowestDeaths = 0;
+    public float BestTimeSeconds = float.MaxValue;
+    public float TotalTimeSeconds = 0f;
+    public bool Unlocked = false;
+    public bool Completed = false;
+
+}
+}
