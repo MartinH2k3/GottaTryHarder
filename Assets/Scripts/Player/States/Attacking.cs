780a4e66f35d389249995769a32b669c544cc131
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-10-22 11:33:18 +0200

    feat: attack delay implemented + performance fix where collider array doesn't get reallocated on each call

--- Assets/Scripts/Player/States/Attacking.cs
+++ Assets/Scripts/Player/States/Attacking.cs
@@ -9,13 +9,29 @@ public class Attacking: PlayerState
 {
     public bool IsAttackFinished => P.animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f;
     private CombatStats _stats;
+    private float _attackStartTime;
+    private bool _attackExecuted;
+
+    private Collider2D[] _hits = new Collider2D[8];
+    private ContactFilter2D _targetFilter = new ContactFilter2D();
 
     public Attacking(PlayerController p) : base(p) {
         _stats = P.combatStats;
+        _targetFilter.layerMask = P.attackableLayer;
     }
 
     public override void Enter() {
         P.animator.SetTrigger("Attack");
+
+        _attackStartTime = Time.time + _stats.attackDelay;
+        _attackExecuted = false;
+    }
+
+    public override void Tick() {
+        if (Time.time < _attackStartTime || _attackExecuted) {
+            return;
+        }
+
         if (!_stats.comboUnlocked) {
             Attack(AttackType.LeftJab);
         }
@@ -39,19 +55,29 @@ public class Attacking: PlayerState
 
             P.AdvanceCombo();
         }
+
+        _attackExecuted = true;
     }
 
     private void Attack(AttackType attackType) {
         var bottomLeft = (Vector2)P.transform.position + new Vector2(0, -_stats.attackHeight);
         var topRight = (Vector2)P.transform.position + new Vector2(_stats.attackRange * P.FacingDirection, _stats.attackHeight);
 
-        Collider2D[] hits = Physics2D.OverlapAreaAll(bottomLeft, topRight, P.attackableLayer);
+        int count = Physics2D.OverlapArea(bottomLeft, topRight, _targetFilter, _hits);
 
-        foreach (var collider in hits) {
+        if (count > _hits.Length) {
+            _hits = new Collider2D[count + _hits.Length];
+            count = Physics2D.OverlapArea(bottomLeft, topRight, _targetFilter, _hits);
+        }
+
+        for (int i = 0; i < count; i++) {
+            var collider = _hits[i];
+
             var attackable = collider.GetComponent<IAttackable>();
-            if (attackable is not null) {
-                attackable.TakeDamage(_stats.attackDamage);
-            }
+            if (attackable is null)
+                continue;
+            attackable.TakeDamage(_stats.attackDamage);
+
             // if physics object, apply force
             var physicsBody = collider.GetComponent<IPhysicsMovable>();
             if (physicsBody is not null) {
--- Assets/Scripts/Player/Stats/CombatStats.cs
+++ Assets/Scripts/Player/Stats/CombatStats.cs
@@ -9,12 +9,13 @@ public class CombatStats
     public float jumpKickTime = 0.25f; // time window after jumping to do a jump kick
     public float jumpKickDamageMultiplier = 1.25f; // attack damage * this
     public int attackDamage = 10;
-    public float attackRange = 0.5f;
+    public float attackRange = 1f;
     public float attackHeight = 0.2f;
     public float attackWidth = 0.5f;
-    public float attackDelay = 0.1f; // Delay from animation trigger to actual attack
     public float attackKnockback = 2f;
     public float attackRate = 2f; // attacks per second
+
+    public float attackDelay = 0.1f; // Delay from animation trigger to actual attack
 
     public bool jumpKickUnlocked = false;
     public bool comboUnlocked = false;
