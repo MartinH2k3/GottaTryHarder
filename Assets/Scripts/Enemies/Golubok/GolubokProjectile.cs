5ce122c66828c1d52f17dea35c4ad48d30d6e7a3
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-12-14 23:28:26 +0100

    shit updated to do knockback

--- Assets/Scripts/Enemies/Golubok/GolubokProjectile.cs
+++ Assets/Scripts/Enemies/Golubok/GolubokProjectile.cs
@@ -7,14 +7,18 @@ namespace Enemies.Golubok
 public class GolubokProjectile: Poolable
 {
     [SerializeField] private int damage = 5;
+    [SerializeField] private float knockback = 1f;
     [SerializeField] private LayerMask playerLayer;
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (((1 << other.gameObject.layer) & playerLayer) != 0) {
             var player = other.gameObject.GetComponent<Player.PlayerController>();
+            if (player == null) return;
 
-            if (player != null)
-                player.TakeDamage(damage);
+            Vector2 knockbackDir = (other.transform.position - transform.position).x > 0 ? Vector2.right : Vector2.left;
+            player.AddForce(knockbackDir * knockback, ForceMode2D.Impulse);
+            player.TakeDamage(damage);
+
         }
         Despawn();
     }
