373c50248a25f5998555df231440791e39bd7ab6
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-12-18 16:13:12 +0100

    Tiles moved around + started working on the car

---
+++ Assets/Scripts/OtherObjects/Car.cs
@@ -0,0 +1,7 @@
+﻿namespace OtherObjects
+{
+public class Car
+{
+    
+}
+}
---
+++ Assets/Scripts/OtherObjects/CarSpawner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace OtherObjects
+{
+public class CarSpawner: MonoBehaviour
+{
+    [SerializeField] private GameObject carPrefab;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float spawnInterval = 5f;
+}
+}
